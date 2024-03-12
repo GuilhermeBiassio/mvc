86d52f5b0f15ff66fa6baaa258b4daf6cb9349f0
Guilherme Biassio <guilherme.cruz.biassio@gmail.com>
2024-03-12 20:38:06 -0300

    start project

---
+++ app/controller/pages/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Pages;
+
+
+class Home
+{
+
+    /**
+    * Return the home view content
+    * @return string
+    */
+    public static function getHome(){
+        return "Hello, world!";
+    }
+}
